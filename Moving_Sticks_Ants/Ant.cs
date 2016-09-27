f7781983370c3bd1077188f1877468a260e29828
JKVX <357800013@qq.com>
2016-09-27 14:26:15 +0800

    1

--- Moving_Sticks_Ants/Ant.cs
+++ Moving_Sticks_Ants/Ant.cs
@@ -8,8 +8,60 @@ namespace Moving_Sticks_Ants
 {
     class Ant
     {
-        private int direction;//0 左 1右
+        private int direction;//-1 左 1右
         private double speed;
         private double position;
+        private int status;//0爬出去了 1未爬出
+
+        public double Position
+        {
+            get
+            {
+                return position;
+            }
+
+            set
+            {
+                position = value;
+            }
+        }
+
+        public double Speed
+        {
+            get
+            {
+                return speed;
+            }
+
+            set
+            {
+                speed = value;
+            }
+        }
+
+        public int Direction
+        {
+            get
+            {
+                return direction;
+            }
+
+            set
+            {
+                direction = value;
+            }
+        }
+
+        Ant(int d,double s,double p)
+        {
+            Direction = d;
+            Speed = s;
+            Position = p;
+        }
+
+        public void creep(double time)
+        {
+            Position = Position + time * speed * direction;
+        }
     }
 }
